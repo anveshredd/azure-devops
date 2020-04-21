40701ff790e11852ae95ae315ad02d64ca2846d4
anveshredd <anvesh09reddy@gmail.com>
2020-04-21 14:39:15 -0400

    Update provider.tf

--- 1-terraform/provider.tf
+++ 1-terraform/provider.tf
@@ -1,5 +1,5 @@
 # Configure the Azure Provider
 provider "azurerm" {
   # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "2.0.0"
+  version = "1.44.0"
 }
